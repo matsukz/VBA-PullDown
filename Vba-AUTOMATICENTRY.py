79cd7d60344c5c3ef9fde1c9806b20a4e9e0c1ca
まつ <92625267+matsukz@users.noreply.github.com>
2022-11-01 15:41:36 +0900

    Update Vba-AUTOMATICENTRY.py

--- Vba-AUTOMATICENTRY.py
+++ Vba-AUTOMATICENTRY.py
@@ -43,7 +43,13 @@ delckb.set(True)
 
 sCount = 0
 
-FXlist = ("Sub", "Range", "AutoFill", "Dim", "MessageBox")
+FXlist = (
+    "Sub",
+    "Range",
+    "AutoFill",
+    "Dim",
+    "MessageBox",
+    "Worksheets")
 combobox = ttk.Combobox(
     root, 
     values=FXlist, 
@@ -54,6 +60,7 @@ combobox = ttk.Combobox(
 combobox.set(FXlist[0])
 
 def exe():
+    print(combobox.get())
 
     msell = ""
     osell = ""
@@ -61,7 +68,7 @@ def exe():
     msell = mtxtbox.get("1.0", "end-1c")
     osell = otxtbox.get("1.0", "end-1c")
     
-    if not msell == "" :
+    if not msell == "":
 
         pyautogui.click(48, 0)
         if combobox.get() == "Range":
@@ -74,21 +81,22 @@ def exe():
                 pyautogui.hotkey("ctrl", "v")
                 pyautogui.write(Rmain)
 
-                if not osell=="" :
+                if not osell=="":
                     pyperclip.copy(osell)
                     pyautogui.hotkey("ctrl", "v")
-                else :
+                else:
                         pass
 
                 pyautogui.press("Return")
 
-            except Exception as e :
+            except Exception as e:
                 tkinter.messagebox.showerror("ERROR", "範囲：Range")
 
         elif combobox.get()=="Sub":
             try:
                 Copy = ""
                 pyperclip.copy(msell)
+                print(Copy)
 
                 pyautogui.write("Sub ")
                 pyautogui.hotkey("ctrl", "v")
@@ -97,12 +105,12 @@ def exe():
                 pyautogui.press("Return")
                 pyautogui.press("Tab")
 
-                if mckb.get()==True :
+                if mckb.get()==True:
                     pyautogui.write("Cells.delet")
                 
                 pyautogui.press("Return")
             
-            except Exception as e :
+            except Exception as e:
                 tkinter.messagebox.showerror("ERROR", "範囲：sub")
 
         elif combobox.get()=="AutoFill":
@@ -127,7 +135,7 @@ def exe():
                 pyautogui.write(Rmain)
                 pyautogui.press("Return")              
             
-            except Exception as e :
+            except Exception as e:
                 tkinter.messagebox.showerror("ERROR", "範囲：AutoFill")
             
         elif combobox.get() == "Dim":
@@ -137,21 +145,21 @@ def exe():
                 pyperclip.copy(msell)
                 pyautogui.hotkey("ctrl", "v")
 
-                if not osell == "" :
+                if not osell == "":
                     pyautogui.write(" As " + osell)
-                else :
+                else:
                     pass
                 
                 pyautogui.press("Return")
 
-            except Exception as e :
+            except Exception as e:
                 tkinter.messagebox.showerror("ERROR", "範囲：Dim")
             
-            if delckb.get() == True :
+            if delckb.get() == True:
                 mtxtbox.delete("1.0", "end-1c")
                 otxtbox.delete("1.0", "end-1c")
 
-        elif combobox.get() == "MessageBox" :
+        elif combobox.get() == "MessageBox":
             try:
                 pyautogui.write("Msgbox \"")
                 pyperclip.copy(msell)
@@ -159,8 +167,24 @@ def exe():
                 pyautogui.write("\"")
                 pyautogui.press("Return")
             
-            except Exception as e :
+            except Exception as e:
                 tkinter.messagebox.showerror("ERROR", "範囲：MessageBox")
+        elif combobox.get() == "Worksheets":
+            try:
+                pyautogui.write("worksheets(\"")
+                pyperclip.copy(msell)
+                pyautogui.hotkey("ctrl", "v")
+                pyautogui.write("\")")
+                if not osell == "":
+                    pyperclip.copy(osell)
+                    pyautogui.hotkey("ctrl", "v")
+                else:
+                    pass
+
+                pyautogui.press("Return")
+
+            except Exception as e:
+                tkinter.messagebox.showerror("ERROR", "範囲：worksheets")
         else:
             tkinter.messagebox.showerror("ERROR", "Combobox RANGE ERROR")
 
@@ -188,7 +212,7 @@ def Hojyo():
     global sCount
     sRadio = tkinter.IntVar()
     sRadio.set(0)
-    if sCount == 0 :
+    if sCount == 0:
         sCount = sCount + 1
         sWindow = tkinter.Toplevel()
 
@@ -201,12 +225,10 @@ def Hojyo():
         FRadio = tkinter.Radiobutton(
             sWindow,
             value=0,
-            variable=sRadio
+            variable=sRadio,
+            text="色の設定"
         )
         FRadio.place(x=10, y=30)
-
-        sfont = tkinter.Label(sWindow, text="色の設定")
-        sfont.place(x=30, y=30)
 
         Flist = ("文字の色", "セルの色")
         Fcombobox = ttk.Combobox(
@@ -234,12 +256,10 @@ def Hojyo():
         DRadio = tkinter.Radiobutton(
             sWindow,
             value=1,
-            variable=sRadio
+            variable=sRadio,
+            text="変数の型"
         )
         DRadio.place(x=10, y=60)
-
-        sdim = tkinter.Label(sWindow, text="変数の型")
-        sdim.place(x=30, y=60)
 
         Dlist = (
             "長整数型", 
@@ -264,41 +284,42 @@ def Hojyo():
             global combobox
             otxtbox.delete("1.0", "end-1c")
 
-            if sRadio.get() == 0 :
+            if sRadio.get() == 0:
 
-                if Fcombobox.get() == "文字の色" :
+                if Fcombobox.get() == "文字の色":
                     otxtbox.insert("1.0", ".font.colorindex=" + str(cIquB.get("1.0", "end-1c")))
                     combobox.set(FXlist[1])
-                elif Fcombobox.get() == "セルの色" :
+                elif Fcombobox.get() == "セルの色":
                     otxtbox.insert("1.0", ".Interior.colorindex=" + str(cIquB.get("1.0", "end-1c")))
                     combobox.set(FXlist[1])
                 else:
                     tkinter.messagebox.showerror("ERROR", "範囲：mainclip Fcombobox sRadio")
 
-            if sRadio.get() == 1 :
+            if sRadio.get() == 1:
 
                 if Dcombobox.get() == "長整数型":
                     otxtbox.insert("1.0", "Long")
                     combobox.set(FXlist[3])
 
-                elif Dcombobox.get() == "倍精度浮動小数点型" :
+                elif Dcombobox.get() == "倍精度浮動小数点型":
                     otxtbox.insert("1.0", "Double")
                     combobox.set(FXlist[3])
 
-                elif Dcombobox.get() == "文字列型" :
+                elif Dcombobox.get() == "文字列型":
                     otxtbox.insert("1.0", "String")
                     combobox.set(FXlist[3])
 
-                elif Dcombobox.get() == "ALLデータ型" :
+                elif Dcombobox.get() == "ALLデータ型":
                     otxtbox.insert("1.0", "Variant")
                     combobox.set(FXlist[3])
 
-                else :
+                else:
                     tkinter.messagebox.showerror("ERROR", "範囲：mainclip Dcombobox sRando")
 
         def close():
             global sCount
             sCount = sCount - 1
+            print(sCount)
             sWindow.destroy()
 
         sWindow.protocol("WM_DELETE_WINDOW", close)
@@ -307,6 +328,7 @@ def Hojyo():
         mainclip_button.place(x=100,y=130)
 
     else:
+        print("sCount ERROR!! sCount is " + str(sCount))
         pass
 
 Execute_button = tkinter.Button(text="Execute",command=exe,width=16,height=3)
